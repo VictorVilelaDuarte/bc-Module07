2fa3fecd813a98d407f0bf5e25eeda7bdce9c41b
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-11-14 01:17:28 -0300

    Configuração e consumo de API com json-server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,101 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          {' '}
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          {' '}
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          {' '}
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          {' '}
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          {' '}
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-        <button type="button">
-          {' '}
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              {' '}
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />3
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
