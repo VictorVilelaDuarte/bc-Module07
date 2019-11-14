32cdfccaa96d04a3dffdf391d9ef80874511196b
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-11-13 23:35:26 -0300

    Estilização da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          {' '}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          {' '}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          {' '}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          {' '}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          {' '}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-reebok-legacy-lifter-crossfit-power-weight-lifting-lpo/06/D19-3706-006/D19-3706-006_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          {' '}
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
